60421fcef6eca858917f1a799b3f8e87c92710f3
Jhonatan reynel hernandez <Jhonatan.reynel814@pascualbravo.edu.co>
2026-06-18 18:11:14 -0500

    nuevos cambios

--- src/pages/Amigos.jsx
+++ src/pages/Amigos.jsx
@@ -21,7 +21,7 @@ export default function Amigos({ onCerrar, embebido }) {
     cargarSolicitudes()
     cargarBloqueados()
     cargarSugeridos()
-  }, [usuario])
+  }, [usuario?.id])
 
   async function cargarSugeridos() {
     setCargandoSugeridos(true)
